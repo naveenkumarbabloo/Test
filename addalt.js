fbab29f61ac809f23bba563ad1591a4fc118dc3b
naveenkumarbabloo <naveen.babloo070@gmail.com>
2017-04-21 14:09:20 -0500

    Add files via upload
    addition, subtraction, multiplication and division.

---
+++ addalt.js
@@ -0,0 +1,12 @@
+var sum = require('./add')
+console.log(sum.add(3,6));	
+
+
+var sub = require('./sub')
+console.log(sub.sub(3,6));	
+
+var mul = require('./mul')
+console.log(mul.mul(3,6));	
+
+var div = require('./div')
+console.log(div.div(3,6));	
